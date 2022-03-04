d1e5f2bd71955424347bf224b59a9c0a0c0fdf7c
theyseemeRobin <100965324+theyseemeRobin@users.noreply.github.com>
2022-03-04 20:47:09 +0100

    Add files via upload

---
+++ speller.c
@@ -0,0 +1,63 @@
+// a SLOW spell checker
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "dictionary.h"
+
+// remove non-alphabetic characters and convert to lower case
+void trimWord(char *word) {
+  int k = 0;
+  for (int i = 0; i < (int) strlen(word); i++) {
+    if (isalpha(word[i])) {
+      word[k] = tolower(word[i]);
+      k++;
+    }
+  }
+  word[k] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+
+  char word[LENGTH + 1] = "";
+
+  // step 1: read in the dictionary
+  dict *dictionary = newEmptyDict();
+  while (scanf("%45s",word) && word[0] != '!') {
+    trimWord(word);
+    addWord(word,dictionary);
+  }
+
+  // step 2: read in text
+  int counter = 0; // number of unknown words
+
+  int index = 0;
+  int c = EOF;
+  while ((c = getchar()) && c != EOF) {
+    if (isalpha(c)){
+      if (c >= 65 && c <= 90){
+        c += 32;
+      }
+
+      word[index] = c;
+      index++;
+    }else{
+      word[index] = '\0';
+      //printf("Word input: %s\n", word);
+      if (index && !check(word,dictionary)) {
+        counter++;
+        printf("%s\n",word);
+      }
+      index = 0;
+    }
+
+  }
+
+
+  // step 3: print number of unknown words
+  printf("%d\n", counter);
+
+  freeDict(dictionary);
+  return 0;
+}
